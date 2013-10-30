8696715b72574e9510b74c6582443fdc6c1b90b3
hilljh82 <hilljh82@gmail.com>
2013-10-30 12:49:16 -0400

    Updated Tool to support forking

--- examples/pintools/ManualExamples/fork_jit_tool.cpp
+++ examples/pintools/ManualExamples/fork_jit_tool.cpp
@@ -12,120 +12,60 @@
 typedef int pid_t;
 #endif
 
-/**
- * @class beforeFork
- *
- * Callback that gets parent pids before fork calls.
- */
-class beforeFork : public OASIS::Pin::Callback <beforeFork (void)>
+class fork_jit_tool : public OASIS::Pin::Tool <fork_jit_tool>
 {
 public:
-  beforeFork (OASIS::Pin::Lock & lock, pid_t & parent_pid)
-  : lock_ (lock),
-    parent_pid_ (parent_pid) { }
-
-  void handle_analyze_fork (THREADID thr_id, const OASIS::Pin::Const_Context & ctx)
+  fork_jit_tool (void)
+  {
+    this->enable_fork_before_callback ();
+    this->enable_fork_after_in_child_callback ();
+    this->enable_fork_after_in_parent_callback ();
+  }
+  
+  void handle_fork_before (THREADID thr_id, const OASIS::Pin::Const_Context &)
   {
     do
     {
       OASIS::Pin::Guard <OASIS::Pin::Lock> (this->lock_, thr_id + 1);
       std::cerr << "TOOL: Before fork." << std::endl;
     } while (false);
-
+    
     this->parent_pid_ = PIN_GetPid ();
   }
-
-private:
-  OASIS::Pin::Lock & lock_;
-  pid_t & parent_pid_;
-};
-
-/**
- * @class afterForkInParent
- *
- * Callback that verifies parent PIDs
- */
-class afterForkInParent : public OASIS::Pin::Callback <afterForkInParent (void)>
-{
-public:
-  afterForkInParent (OASIS::Pin::Lock & lock, pid_t & parent_pid)
-  : lock_ (lock),
-    parent_pid_ (parent_pid) { }
-
-  void handle_analyze_fork (THREADID thr_id, const OASIS::Pin::Const_Context & ctx)
-  {
-    do
-    {
-      OASIS::Pin::Guard <OASIS::Pin::Lock> (this->lock_, thr_id + 1);
-      std::cerr << "TOOL: After fork in parent." << std::endl;
-    } while (false);
-
-    if (PIN_GetPid () != this->parent_pid_)
-    {
-      std::cerr << "PIN_GetPid () fails in parent process" << std::endl;
-      exit (-1);
-    }
-  }
-
-private:
-  OASIS::Pin::Lock & lock_;
-
-  pid_t & parent_pid_;
-};
-
-/**
- * @class afterForkInChild
- *
- * Callback that verifies child PIDs
- */
-class afterForkInChild : public OASIS::Pin::Callback <afterForkInChild (void)>
-{
-public:
-  afterForkInChild (OASIS::Pin::Lock & lock, pid_t & parent_pid)
-  : lock_ (lock),
-    parent_pid_ (parent_pid) { }
-
-  void handle_analyze_fork (THREADID thr_id, const OASIS::Pin::Const_Context & ctx)
+  
+  void handle_fork_after_in_child (THREADID thr_id, const OASIS::Pin::Const_Context &)
   {
     do
     {
       OASIS::Pin::Guard <OASIS::Pin::Lock> (this->lock_, thr_id + 1);
       std::cerr << "TOOL: After fork in child." << std::endl;
     } while (false);
-
+    
     if ((PIN_GetPid () == this->parent_pid_) || (getppid () != this->parent_pid_))
     {
       std::cerr << "PIN_GetPid () failes in child process" << std::endl;
       exit (-1);
     }
   }
-
-private:
-  OASIS::Pin::Lock & lock_;
-  pid_t & parent_pid_;
-};
-
-class fork_jit_tool : public OASIS::Pin::Tool <fork_jit_tool>
-{
-public:
-  fork_jit_tool (void)
-  : before_ (lock_, parent_pid_),
-    after_parent_ (lock_, parent_pid_),
-    after_child_ (lock_, parent_pid_)
+  
+  void handle_fork_after_in_parent (THREADID thr_id, const OASIS::Pin::Const_Context &)
   {
-    this->add_fork_function (FPOINT_BEFORE, &this->before_);
-    this->add_fork_function (FPOINT_AFTER_IN_PARENT, &this->after_parent_);
-    this->add_fork_function (FPOINT_AFTER_IN_CHILD, &this->after_child_);
+    do
+    {
+      OASIS::Pin::Guard <OASIS::Pin::Lock> (this->lock_, thr_id + 1);
+      std::cerr << "TOOL: After fork in parent." << std::endl;
+    } while (false);
+    
+    if (PIN_GetPid () != this->parent_pid_)
+    {
+      std::cerr << "PIN_GetPid () fails in parent process" << std::endl;
+      exit (-1);
+    }    
   }
 
 private:
   OASIS::Pin::Lock lock_;
   pid_t parent_pid_;
-
-  // The callbacks
-  beforeFork before_;
-  afterForkInParent after_parent_;
-  afterForkInChild after_child_;
 };
 
 DECLARE_PINTOOL (fork_jit_tool)
