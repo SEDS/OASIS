bb0a09f29b434e15f64746545b10b554e7c4aab6
Dennis Feiock <dfeiock at iupui dot edu>
2013-11-12 15:13:01 -0500

    Fixed typo

--- examples/pintools/CoolTools/libloader.cpp
+++ examples/pintools/CoolTools/libloader.cpp
@@ -11,11 +11,11 @@ int main (int argc, char * argv [])
 {
   for (int i = 1; i < argc ; ++i)
   {
-    std::cout << "Loading libaray: " << argv[i] << std::endl;
+    std::cout << "Loading library: " << argv[i] << std::endl;
     void * handle = 0;
     handle = dlopen (argv[i], RTLD_NOW);
     if (handle == 0)
-      std::cout << "Failed to load libaray " << argv[i] << std::endl;
+      std::cout << "Failed to load library: " << argv[i] << std::endl;
   }
 
   return 0; 
